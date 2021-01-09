5a2bca2b23ce32f04002d6d55fd649d1cef454b8
1Soyebo <soyeboibukun@gmail.com>
2021-01-09 15:31:51 +0100

    First Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ibukunoluwa Soyebo on 09/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Text("Hello World")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
